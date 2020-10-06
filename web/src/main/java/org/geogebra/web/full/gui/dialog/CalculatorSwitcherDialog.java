5f0fb1adef58a06ed1ac7534b1271acbc7e7e678
agoston <agoston@geogebra.org>
2020-10-06 16:17:14 +0200

    tmp 2

--- web/src/main/java/org/geogebra/web/full/gui/dialog/CalculatorSwitcherDialog.java
+++ web/src/main/java/org/geogebra/web/full/gui/dialog/CalculatorSwitcherDialog.java
@@ -1,9 +1,15 @@
 package org.geogebra.web.full.gui.dialog;
 
+import org.geogebra.common.GeoGebraConstants;
+import org.geogebra.common.io.layout.PerspectiveDecoder;
+import org.geogebra.common.kernel.arithmetic.SymbolicMode;
+import org.geogebra.web.full.gui.GuiManagerW;
 import org.geogebra.web.full.gui.images.SvgPerspectiveResources;
+import org.geogebra.web.full.main.AppWFull;
 import org.geogebra.web.html5.gui.GPopupPanel;
 import org.geogebra.web.html5.gui.view.button.StandardButton;
 import org.geogebra.web.html5.main.AppW;
+import org.geogebra.web.html5.util.LoadFilePresenter;
 import org.geogebra.web.html5.util.Persistable;
 import org.geogebra.web.resources.SVGResource;
 
@@ -17,8 +23,6 @@ import com.google.gwt.user.client.ui.Label;
  * Calculator chooser for suite
  */
 public class CalculatorSwitcherDialog extends GPopupPanel implements Persistable, ResizeHandler {
-
-	private StandardButton selectedBtn;
 
 	/**
 	 * constructor
@@ -40,19 +44,19 @@ public class CalculatorSwitcherDialog extends GPopupPanel implements Persistable
 
 		SvgPerspectiveResources res = SvgPerspectiveResources.INSTANCE;
 		StandardButton btnGraphing = buildCalcButton(res.menu_icon_algebra_transparent(),
-				"graphing", "GraphingCalculator.short");
+				GeoGebraConstants.GRAPHING_APPCODE, "GraphingCalculator.short");
 		contentPanel.add(btnGraphing);
 
 		StandardButton btn3D = buildCalcButton(res.menu_icon_graphics3D_transparent(),
-				"3d", "GeoGebra3DGrapher.short");
+				GeoGebraConstants.G3D_APPCODE, "GeoGebra3DGrapher.short");
 		contentPanel.add(btn3D);
 
 		StandardButton btnGeometry = buildCalcButton(res.menu_icon_geometry_transparent(),
-				"geometry", "Geometry");
+				GeoGebraConstants.GEOMETRY_APPCODE, "Geometry");
 		contentPanel.add(btnGeometry);
 
 		StandardButton btnCAS = buildCalcButton(res.cas_white_bg(),
-				"cas", "CAS");
+				GeoGebraConstants.CAS_APPCODE, "CAS");
 		contentPanel.add(btnCAS);
 
 		add(contentPanel);
@@ -60,7 +64,7 @@ public class CalculatorSwitcherDialog extends GPopupPanel implements Persistable
 
 	private StandardButton buildCalcButton(SVGResource icon, String subAppCode,
 			String appNameKey) {
-		 StandardButton button =  new StandardButton(app, 72, icon,
+		StandardButton button =  new StandardButton(app, 72, icon,
 				 app.getLocalization().getMenu(appNameKey));
 		button.setStyleName("calcBtn");
 		if (subAppCode.equals(app.getConfig().getSubAppCode())) {
@@ -69,8 +73,13 @@ public class CalculatorSwitcherDialog extends GPopupPanel implements Persistable
 
 		button.addFastClickHandler(source -> {
 			hide();
-			app.getConfig().setSubAppCode(subAppCode);
-			((AppW) app).resetUI();
+			AppWFull appWFull = ((AppWFull) app);
+			appWFull.resetActivity(subAppCode);
+			appWFull.getKernel().setSymbolicMode(
+					GeoGebraConstants.CAS_APPCODE.equals(subAppCode)
+							? SymbolicMode.SYMBOLIC_AV
+							: SymbolicMode.NONE);
+			appWFull.getGgbApi().setPerspective(app.getConfig().getForcedPerspective());
 		});
 		return button;
 	}
